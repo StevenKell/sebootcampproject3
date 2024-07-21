eca87ac7c9d72a032bb5b6550d5d00d919af4279
StevenKell <steventkell@gmail.com>
2024-07-21 17:13:47 -0400

    More work on frontend, elements have been moved and resized. Now look closer to project images.

--- scripts.js
+++ scripts.js
@@ -3,12 +3,14 @@ console.log(rondomNum);
 
 let currentScore=10;
 let historyHighScore=0;
+const guesshistory = [];
 
 let checkbtn=document.querySelector('.checkbtn')
 let reset=document.querySelector('.resetbtn');
 let msg=document.querySelector('p')
 let userInput=document.querySelector('input');
 let currntScore=document.querySelector('.crrntscore');
+
 
 checkbtn.addEventListener('click',function(){
     
@@ -30,7 +32,9 @@ if(currentScore>0){
             msg.style.color='yellow';
             msg.style.fontSize='34px';
             currentScore=currentScore-1;
-            currntScore.textContent=String(currentScore);    
+            currntScore.textContent=String(currentScore);
+            guesshistory.push(userInput); 
+            console.log(guesshistory);   
         }
         if(Number(userInput.value)<rondomNum){
             msg.textContent='Wrong number! too low! try a larger value';
@@ -38,6 +42,9 @@ if(currentScore>0){
             msg.style.fontSize='34px';
             currentScore=currentScore-1;
             currntScore.textContent=String(currentScore);
+            guesshistory.push(userInput)    
+            console.log(guesshistory)   
+
         }
         if(currentScore===0){
             msg.textContent='You lose! Play again!';
